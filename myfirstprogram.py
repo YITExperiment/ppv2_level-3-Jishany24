f50f603920b4e9ebafe601961ef569cfc509ca34
Jishany24 <135231821+Jishany24@users.noreply.github.com>
2023-06-05 12:05:24 +0530

    Add files via upload

---
+++ myfirstprogram.py
@@ -0,0 +1,13 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:24:28) [MSC v.1934 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================================================== RESTART: C:/Users/Nilojana/AppData/Local/Programs/Python/Python311-32/my new.py ===================================================
+Hello, World!
+>>> 
+=================================================== RESTART: C:/Users/Nilojana/AppData/Local/Programs/Python/Python311-32/my new 1.py ==================================================
+Hello, World!
+What is your name? Jiahani
+Hello  Jiahani
+>>> 
+===================================================== RESTART: C:/Users/Nilojana/AppData/Local/Programs/Python/Python311-32/age.py =====================================================
+12
