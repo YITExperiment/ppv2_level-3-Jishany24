0332c381153fbede73e0b84ec5226451fca284da
Jishany24 <135231821+Jishany24@users.noreply.github.com>
2023-06-05 12:02:50 +0530

    Add files via upload

---
+++ kaleido_spiral.py
@@ -0,0 +1,28 @@
+import turtle
+from itertools import cycle
+colors=cycle(['red','orange','yellow','green','blue','purple'])
+
+
+def draw_circle(size,angle,shift):
+    turtle.bgcolor(next(colors))
+    turtle.circle(size)
+    turtle.pencolor(next(colors))
+    turtle.circle(size)
+    turtle.right(angle)
+    turtle.forward(shift)
+    draw_circle(size+5,angle+1,shift+1)
+
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(40)
+turtle.pencolor('red')
+turtle.circle(30,0,1)
+
+
+
+
+
+
+
+
+
